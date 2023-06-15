44d08fbd68f65a2d566ea7a31207d8441d5a18de
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-06-15 21:45:29 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import {Home, Team, Blogs, Blog, NotFound, Projects} from "./Pages";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+      <div className="main__center">
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/team' element={<Team/>}/>
+          <Route exact path='/blogs' element={<Blogs/>}/>
+          <Route exact path='/blog/:id' element={<Blog/>}/>
+          <Route exact path='*' element={<NotFound/>}/>
+          <Route exact path='/projects' element={<Projects/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from "./Home/Home";
+export {default as Team} from "./Team/Team";
+export {default as Blogs} from "./Blogs/Blogs";
+export {default as Blog} from "./Blog/Blog";
+export {default as Projects} from "./Projects/Projects";
+export {default as NotFound} from "./PageNotFound/NotFound";
