c29d463a0f528171d99b3c6902ccfd20383d8535
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-06-18 17:58:10 +0530

    page not found

--- src/Pages/PageNotFound/NotFound.jsx
+++ src/Pages/PageNotFound/NotFound.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from "./NotFound.module.css";
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+
+  const {page__error} = styles;
+
   return (
-    <div>NotFound</div>
+    <div className={page__error}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <Link to='/'><ion-icon name='chevron-back'></ion-icon> <span>Back Home</span></Link>
+    </div>
   )
 }
 
